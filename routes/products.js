5f7ffdb88dc5c294ef926f17be760b3624c8f979
PamelaSohWY <pamelasohwy@gmail.com>
2021-07-06 08:16:52 +0000

    lab 4

--- routes/products.js
+++ routes/products.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // #1 import in the Product Model 
 const {Product} = require('../models')
 
-router.get('/', async(req.res) => {
+router.get('/', async (req, res) => {
 // #2 - fetch all the products (ie. select * from products)
 let products = await Product.collection().fetch();
 res.render('products/index',{
