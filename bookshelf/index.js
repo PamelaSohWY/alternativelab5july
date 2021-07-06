d53eeb66af7f43c4638bad1007da5659a6004162
PamelaSohWY <pamelasohwy@gmail.com>
2021-07-06 07:00:44 +0000

    lab 3 done. but need to check on products.js

---
+++ bookshelf/index.js
@@ -0,0 +1,14 @@
+//setting up the database connection 
+
+const knex = require ('knex')({
+client:'mysql',
+connection:{
+user:'foo',
+password:'bar',
+database:'organic'
+}
+})
+
+const bookshelf = require('bookshelf')(knex)
+
+module.exports = bookshelf;
